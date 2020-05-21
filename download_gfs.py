589e52cc6d61c8b8657a006f2330be775c49dedd
Bielorusse <thibautvoirand@gmail.com>
2020-05-21 11:27:33 +0200

    moved extent to main script, added cleaning option

--- download_gfs.py
+++ download_gfs.py
@@ -2,15 +2,13 @@
 GFS data downloading module for Duventchezmoi.
 """
 
+# standard packages
 import os
 import urllib.request
 import datetime
-import math
 
 
-def create_gfs_url(
-    run_date, extent, forecast_hours
-):
+def create_gfs_url(run_date, extent, forecast_hours):
     """
     Generates GFS data request.
     Input:
@@ -33,10 +31,7 @@ def create_gfs_url(
     # creating url
     url = "https://nomads.ncep.noaa.gov"
     url += "/cgi-bin/filter_gfs_{0}.pl?file=gfs.t{1}z.pgrb2.{2}.f{3}".format(
-        grid,
-        step,
-        grid,
-        forecast_hours
+        grid, step, grid, forecast_hours
     )
     for level in levels:
         url += "&lev_{}=on".format(level)
@@ -79,13 +74,12 @@ def download_from_url(input_url, output_file, verbose=False):
         return False
 
 
-
-def download_gfs(coordinates, data_path):
+def download_gfs(extent, data_path):
     """
     Download today's GFS forecast for given coordinates.
     Input:
-        -coordinates    [float, float]
-            lat, lon
+        -extent         [float, float, float, float]
+            W, N, E, S, at 0.25 deg precision
         -data_path      str
     """
 
@@ -93,21 +87,13 @@ def download_gfs(coordinates, data_path):
     run_date_str = datetime.datetime.now().strftime("%Y%m%d")
     run_date_obj = datetime.datetime.strptime(run_date_str, "%Y%m%d")
 
-    # create extent on 0.25 deg grid around given coordinates
-    extent = [
-        math.floor(coordinates[1] * 4) / 4,    # smallest lon (W bound)
-        math.ceil(coordinates[0] * 4) / 4,     # greatest lat (N bound)
-        math.ceil(coordinates[1] * 4) / 4,     # greatest lon (E bound)
-        math.floor(coordinates[0] * 4) / 4,    # smallest lat (S bound)
-    ]
-
     # GFS performs 16 days forecast
-    GFS_max_forecast_hours = 72#384
+    GFS_max_forecast_hours = 3#72#384
 
     for forecast_hours in range(GFS_max_forecast_hours + 1):
 
         # create output filename
-        forecast_date = run_date_obj + datetime.timedelta(hours = forecast_hours)
+        forecast_date = run_date_obj + datetime.timedelta(hours=forecast_hours)
         output_file = "{}.grib2".format(forecast_date.strftime("%Y%m%d_%H%S"))
 
         # create URL
@@ -116,12 +102,10 @@ def download_gfs(coordinates, data_path):
         # download file
         download_from_url(url, os.path.join(data_path, output_file), verbose=True)
 
-    return 0
-
 
 if __name__ == "__main__":
 
-    coordinates = [44.834546, -0.566572]
+    extent = [-0.75, 45.0, -0.50, 44.75]
     data_path = "/Users/thibautvoirand/creation/programmation/duventchezmoi/data"
 
-    print(download_gfs(coordinates, data_path))
+    download_gfs(extent, data_path)
--- duventchezmoi.py
+++ duventchezmoi.py
@@ -2,8 +2,17 @@
 Duventchezmoi main script.
 """
 
+# standard packages
 import os
+import sys
+import shutil
 import configparser
+import math
+import datetime
+
+# local modules
+from download_gfs import download_gfs
+
 
 def duventchezmoi(config_path):
     """
@@ -20,19 +29,48 @@ def duventchezmoi(config_path):
     threshold = float(config["main"]["threshold"])
     data_path = config["main"]["data_path"]
     email = config["main"]["email"]
+    cleaning = config["main"]["cleaning"].lower() in ["true"]
+
+    # create extent on 0.25 deg grid around given coordinates
+    extent = [
+        math.floor(lon * 4) / 4,  # smallest lon (W bound)
+        math.ceil(lat * 4) / 4,  # greatest lat (N bound)
+        math.ceil(lon * 4) / 4,  # greatest lon (E bound)
+        math.floor(lat * 4) / 4,  # smallest lat (S bound)
+    ]
+
+    # creating download directory
+    todays_data_path = os.path.join(
+        data_path, datetime.datetime.now().strftime("%Y%m%d")
+    )
+    if not os.path.exists(todays_data_path):
+        os.makedirs(todays_data_path)
 
     # download gfs data
-    
+    try:
+        download_gfs(extent, todays_data_path)
+    except:
+        sys.exit("Error in GFS data download")
 
     # performe daily average of gfs data
 
     # compare results to threshold
+
+    # clear data path
+    if cleaning:
+        for d in [
+            di
+            for di in os.listdir(data_path)
+            if os.path.isdir(os.path.join(data_path, di))
+        ]:
+            shutil.rmtree(os.path.join(data_path, di))
 
     # if alert triggered
 
     # write report
 
     # send email
+
 
 if __name__ == "__main__":
 
